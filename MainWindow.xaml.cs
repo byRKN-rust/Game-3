99ecf14a0fbaf809c0d4a72d250eb0d8f7e98090
byRKN-rust <xzkak485@gmail.com>
2025-06-29 21:50:32 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace MW_Launcher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
